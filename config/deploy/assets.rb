d442f5432c4a4221fb6f10cd54dc538208046c3c
Unayung Chen <unayung@gmail.com>
2013-12-19 01:11:23 +0800

    deploy files

--- config/deploy/assets.rb
+++ config/deploy/assets.rb
@@ -3,28 +3,44 @@
 # from https://github.com/AF83/capistrano-af83/blob/master/lib/capistrano/af83/deploy/assets.rb
 set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
+# namespace :deploy do
+#   namespace :assets do
+
+#     desc <<-DESC
+#       Run the asset precompilation rake task. You can specify the full path \
+#       to the rake executable by setting the rake variable. You can also \
+#       specify additional environment variables to pass to rake via the \
+#       asset_env variable. The defaults are:
+
+#         set :rake,      "rake"
+#         set :rails_env, "production"
+#         set :asset_env, "RAILS_GROUPS=assets"
+#         set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
+#     DESC
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#     end
+
+#   end
+# end
 namespace :deploy do
   namespace :assets do
-
-    desc <<-DESC
-      Run the asset precompilation rake task. You can specify the full path \
-      to the rake executable by setting the rake variable. You can also \
-      specify additional environment variables to pass to rake via the \
-      asset_env variable. The defaults are:
-
-        set :rake,      "rake"
-        set :rails_env, "production"
-        set :asset_env, "RAILS_GROUPS=assets"
-        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-    DESC
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.diff(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+      begin
+        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
+      rescue
+        err_no = true
+      end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
       end
-    end
-
+   end
   end
 end
