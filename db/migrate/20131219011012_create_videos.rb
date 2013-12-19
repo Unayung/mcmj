c670ca34a98fb7ae24b307cf43273af7228a42b1
Unayung Chen <unayung@gmail.com>
2013-12-19 09:11:54 +0800

    add video model

---
+++ db/migrate/20131219011012_create_videos.rb
@@ -0,0 +1,11 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :url
+      t.string :author
+      t.string :title
+      t.text :note
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218161026) do
+ActiveRecord::Schema.define(version: 20131219011012) do
 
   create_table "users", force: true do |t|
     t.string   "email",                             default: "", null: false
@@ -33,5 +33,14 @@ ActiveRecord::Schema.define(version: 20131218161026) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "videos", force: true do |t|
+    t.string   "url"
+    t.string   "author"
+    t.string   "title"
+    t.text     "note"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
