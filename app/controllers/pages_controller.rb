8189c874a0132282ffc41eaa7047703ae626a397
Unayung Chen <unayung@gmail.com>
2013-12-21 17:33:52 +0800

    paginate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@
 class PagesController < HighVoltage::PagesController
 
   def welcome
-    @videos = Video.paginate(:page => params[:page], :per_page => 5).order("id DESC")
+    @videos = Video.paginate(:page => params[:page], :per_page => 10).order("id DESC")
   end
 
 end
