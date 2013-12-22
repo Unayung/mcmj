9f38020859d38bdd1e7e25d25b6f22e6cdf01dde
Unayung Chen <unayung@gmail.com>
2013-12-23 03:25:09 +0800

    fix author link

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,3 +1,15 @@
 # -*- encoding : utf-8 -*-
 module VideosHelper
+
+  def render_video_author_link(video)
+    s = ""
+    if video.url.present? && video.author.present?
+      s += "by "
+      s += link_to(video.author, video.url, :target => :blank)
+    elsif video.author.present?
+      s += "by "
+      s += video.author
+    end
+    s.html_safe
+  end
 end
